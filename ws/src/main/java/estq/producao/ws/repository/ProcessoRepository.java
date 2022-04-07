6ef36123e99cb748eae5cef170a2dbd01813461c
Milena Milicio <milenamilicio@hotmail.com>
2022-04-06 21:07:36 -0300

    adiciona CRUD básico de processo

---
+++ ws/src/main/java/estq/producao/ws/repository/ProcessoRepository.java
@@ -0,0 +1,7 @@
+package estq.producao.ws.repository;
+
+import estq.producao.ws.domain.Processo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProcessoRepository extends CrudRepository<Processo, Long> {
+}
